a0dc667225cbd906bce454af3a9281aedff038ba
u3568494 <u3568494@connect.hku.hk>
2020-04-19 12:26:13 +0800

    1

---
+++ 1340-Project/1340-Project.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+
+using namespace std;
+
+void battle()
+void stage_1()
+void stage_2{}
+
+int main()
+{
+    string name;
+    cout << "Enter your name: ";
+    cin >> name;
+    cout << name+", you have an important mission.";
+    
+}
+
+
